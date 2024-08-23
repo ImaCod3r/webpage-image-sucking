7116f0ac5e1eee780b646060f0f4efe2dc90b03b
Edson Rodrigues <thecodeverse12@gmail.com>
2024-08-23 12:27:11 +0100

    Error handling

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ url = sys.argv[1]
 os.makedirs('Downloads', exist_ok = True)
 
 def getSoup(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        print(e)
+    else:
+        if response.status_code == 200:
+            return BeautifulSoup(response.text, 'html.parser')
+        print(f'This url cant be reached. Status code: {response.status_code}')
     return False
 
 def getImages(soup):
@@ -24,12 +29,12 @@ def getImages(soup):
 def downloadImage(image_url, index):
     if not image_url.startswith(('http://', 'https://')):
         image_url = requests.compat.urljoin(url, image_url)
-
+    
     img_response = requests.get(image_url)
     
     if img_response.status_code == 200:
         img_name = f'image-{index}.jpg'
-
+        
         with open(os.path.join('Downloads', img_name), 'wb') as file:
             file.write(img_response.content)
         print(f'{img_name} successfully downloaded')
@@ -43,8 +48,6 @@ if __name__ == '__main__':
         for image in getImages(soup):
             index += 1
             downloadImage(image, index)
-    else:
-        print('This url cant be reached')
 
 
 
