a13487bab93f7d4ea19fc825d50efdd258d55787
Edson Rodrigues <thecodeverse12@gmail.com>
2024-08-23 12:51:35 +0100

    Add a function to validate the page url

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ url = sys.argv[1]
 DOWNLOAD_FOLDER = os.path.join(ps.getcwd(), 'Downloads')
 os.makedirs(DOWNLOAD_FOLDER, exist_ok = True)
 
+def validate_url(url):
+    if not validators.url(url):
+        print('Invalid url')
+        return False
+    return True
+
 def getSoup(url):
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status() # raise exception for http error code
-    except requests.exceptions.Timeout:
-        print('The request timed out')
-        return False
-    except requets.exceptions.RequestException as e:
-        print(f'An error occured: {e}')
-        return False
-    else:
-        if response.status_code == 200:
-            return BeautifulSoup(response.text, 'html.parser')
+    if validate_url(url):
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status() # raise exception for http error code
+        except requests.exceptions.Timeout:
+            print('The request timed out')
+            return False
+        except requets.exceptions.RequestException as e:
+            print(f'An error occured: {e}')
+            return False
+        else:
+            if response.status_code == 200:
+                return BeautifulSoup(response.text, 'html.parser')
 
 def getImages(soup):
     try:
@@ -35,7 +42,7 @@ def getImages(soup):
 def downloadImage(image_url, index):
     if not image_url.startswith(('http://', 'https://')):
         image_url = requests.compat.urljoin(url, image_url)
-    
+
     img_response = requests.get(image_url)
     
     if img_response.status_code == 200:
