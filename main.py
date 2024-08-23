d8109f65eb2e8c9a52b76ac16a1dac5480ce762c
Edson Rodrigues <thecodeverse12@gmail.com>
2024-08-23 15:11:36 +0100

    Finishing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
 import os
 from bs4 import BeautifulSoup
-import validators
 import requests
 
+DOWNLOAD_FOLDER = 'Downloads'
 os.makedirs(DOWNLOAD_FOLDER, exist_ok = True)
 
 def parse_args():
@@ -12,31 +12,24 @@ def parse_args():
     parser.add_argument("-d", "--directory", default="Downloads", help="Directory to save images")
     return parser.parse_args()
 
-def validate_url(url):
-    if not validators.url(url):
-        print('Invalid url')
-        return False
-    return True
-
 def getSoup(url):
-    if validate_url(url):
-        try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status() # raise exception for http error code
-        except requests.exceptions.Timeout:
-            print('The request timed out')
-            return False
-        except requets.exceptions.RequestException as e:
-            print(f'An error occured: {e}')
-            return False
-        else:
-            if response.status_code == 200:
-                return BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status() # raise exception for http error code
+    except requests.exceptions.Timeout:
+        print('The request timed out')
+        return False
+    except requets.exceptions.RequestException as e:
+        print(f'An error occured: {e}')
+        return False
+    else:
+        if response.status_code == 200:
+            return BeautifulSoup(response.text, 'html.parser')
 
 def getImages(soup):
-    return [img.get('src') for img in soup.find_all('img')
+    return [img.get('src') for img in soup.find_all('img')]
 
-def downloadImage(image_url, index, directory, base_url):
+def downloadImage(base_url, image_url, directory, index):
     if not image_url.startswith(('http://', 'https://')):
         image_url = requests.compat.urljoin(base_url, image_url)
 
@@ -45,7 +38,8 @@ def downloadImage(image_url, index, directory, base_url):
     if img_response.status_code == 200:
         extension = os.path.splitext(image_url)[1] # Get the file extension
         if not extension:
-        extension = '.jpg' # If there is no extension, assume it as JPG
+            extension = '.jpg' # If there is no extension, assume it as JPG
+        
         img_name = f'image-{index}{extension}'
         img_path = os.path.join(directory, img_name)
         
@@ -59,11 +53,11 @@ def main():
     args = parse_args()
     soup = getSoup(args.url)
     if soup:
-        index = 0
-        for image in getImages(soup):
-            index =+ 1
-            downloadImage(image, args.directory, index)
-
+        images = getImages(soup)
+        index = 1
+        for image in images:
+            downloadImage(args.url, image, args.directory, index)
+            index += 1
 
 if __name__ == '__main__':
     main()
